e8dc9af5f2f97a71117e779a219d0f3366d2a2c4
Yeldos Nurlanov <yeldos.ktl@gmail.com>
2023-05-08 19:17:56 +0500

    live coding with Bill

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [items, setItems] = useState([])
+  const [page, setPage] = useState(1)
+
+  // fetch data
+
+  const fetchItems = async () => {
+    try {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_start=${page}&_limit=20`)
+      const data = await response.json()
+      setItems(prevItems => [...prevItems, ...data])
+      setPage(prevPage => prevPage + 1)
+    } catch (error) {
+      console.log('fetching error', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchItems()
+  }, [])
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const {scrollTop, clientHeight, scrollHeight} = document.documentElement
+      if (scrollTop + clientHeight >= scrollHeight) {
+        fetchItems()
+      }
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [page])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="list">
+      {
+        items.map((item, index) => (
+          <div className='list-item' key={index}>{item.title}</div>
+        ))
+      }
     </div>
   );
 }
